b61a9c0a731e3cd71c808e779d835a6870b0645e
Roger Madjos <roger.madjos@gmail.com>
2018-11-23 09:48:41 +0800

    feat(balane): implement Balance mock service

--- src/balance.ts
+++ src/balance.ts
@@ -1,5 +1,21 @@
+import R from 'ramda';
 import { Rabbit } from './types';
 
-export async function startWorker(rabbit: Rabbit) {
-  await rabbit.createWorker('Balance.Command', async () => true);
+let workers: any[];
+export async function start(
+  rabbit: Rabbit,
+  balances: { account: string; available: number; total: number }[]
+) {
+  workers = await Promise.all([
+    rabbit.createWorker('Balance.Query', async ({ type, data }) => {
+      if (type === 'AvailableBalance') {
+        return R.find(R.propEq('account', data.account))(balances) || null;
+      }
+    }),
+    rabbit.createWorker('Balance.Command', async () => true),
+  ]);
+}
+
+export async function stop() {
+  await Promise.all(workers.map(worker => worker.stop()));
 }
