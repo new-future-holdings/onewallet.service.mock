ca1146ef28a40ed451c0d40456f2c05c945411a4
Roger Madjos <roger.madjos@gmail.com>
2018-11-23 09:49:09 +0800

    build(npm): build latest version

--- build/balance.d.ts
+++ build/balance.d.ts
@@ -1,3 +1,8 @@
 import { Rabbit } from './types';
-export declare function startWorker(rabbit: Rabbit): Promise<void>;
+export declare function start(rabbit: Rabbit, balances: {
+    account: string;
+    available: number;
+    total: number;
+}[]): Promise<void>;
+export declare function stop(): Promise<void>;
 //# sourceMappingURL=balance.d.ts.map
--- build/balance.js
+++ build/balance.js
@@ -1,7 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-async function startWorker(rabbit) {
-    await rabbit.createWorker('Balance.Command', async () => true);
+const ramda_1 = __importDefault(require("ramda"));
+let workers;
+async function start(rabbit, balances) {
+    workers = await Promise.all([
+        rabbit.createWorker('Balance.Query', async ({ type, data }) => {
+            if (type === 'AvailableBalance') {
+                return ramda_1.default.find(ramda_1.default.propEq('account', data.account))(balances) || null;
+            }
+        }),
+        rabbit.createWorker('Balance.Command', async () => true),
+    ]);
 }
-exports.startWorker = startWorker;
+exports.start = start;
+async function stop() {
+    await Promise.all(workers.map(worker => worker.stop()));
+}
+exports.stop = stop;
 //# sourceMappingURL=balance.js.map
