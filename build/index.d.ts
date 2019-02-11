8d9eb21476fa26d96029be99ed7a2f2a6f27b0c9
djansyle <djansyledjans@gmail.com>
2019-02-11 13:16:34 +0800

    feat(mock): export rebate mock

--- build/index.d.ts
+++ build/index.d.ts
@@ -2,6 +2,7 @@ import * as EventStoreMock from './eventstore';
 import * as BalanceMock from './balance';
 import * as AccountMock from './account';
 import * as BankMock from './bank';
+import * as RebateMock from './rebate';
 import { generateEventId, generateId, generateFakeEvent } from './util';
-export { EventStoreMock, BalanceMock, AccountMock, BankMock, generateEventId, generateId, generateFakeEvent, };
+export { EventStoreMock, BalanceMock, AccountMock, RebateMock, BankMock, generateEventId, generateId, generateFakeEvent, };
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@ import * as EventStoreMock from './eventstore';
 import * as BalanceMock from './balance';
 import * as AccountMock from './account';
 import * as BankMock from './bank';
+import * as RebateMock from './rebate';
+
 import { generateEventId, generateId, generateFakeEvent } from './util';
 export {
   EventStoreMock,
   BalanceMock,
   AccountMock,
+  RebateMock,
   BankMock,
   generateEventId,
   generateId,
