c4c58127c2e2edaf484bc59bafc405f302525285
Arjane Lolong <rjn.sachi@gmail.com>
2019-12-20 16:30:38 +0800

    fix: use vendor ID, game code and type

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,29 +1,35 @@
 import R from 'ramda';
 
 import { Rabbit } from './types';
+import generateVendorId from './library/generate-vendor-id';
 
 type Game = {
   id: string;
-  gameCode: string;
-  gameType: string;
+  code: string;
+  type: string;
   vendor: string;
+  vendorCode: string;
   details?: any[];
 };
 
 let workers: any[];
 export async function start(rabbit: Rabbit, games: Game[]) {
   workers = await Promise.all([
-    rabbit.createWorker('Vendor.Query', async ({ type, data }) => {
-      if (type === 'Game') {
-        return (
-          R.find<Game>(
-            game =>
-              game.vendor === data.vendor && game.gameCode === data.gameCode
-          )(games) || null
-        );
-      }
-    }),
-  ]);
+      rabbit.createWorker('Vendor.Query', async ({ type, data }: { type: string; data: Game }) => {
+        if (type === 'Game') {
+          return (
+            R.find<Game>(
+              game => game.vendor === (/^vnd_.+$/.test(data.vendor)
+                ? data.vendor
+                : generateVendorId(data.vendor))
+                    && game.code === data.code,
+            )(games) || null
+          );
+        }
+
+        return null;
+      }),
+    ]);
 }
 
 export async function stop() {
