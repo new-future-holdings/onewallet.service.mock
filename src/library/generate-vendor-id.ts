c8b3c9985fb7ac2767f0b8fd4a6c3c21b8cef49a
rogermadjos <roger@hov.co>
2022-02-10 17:26:02 +0800

    chore: add npm config

--- src/library/generate-vendor-id.ts
+++ src/library/generate-vendor-id.ts
@@ -1,4 +1,4 @@
-import uuidv5 from 'uuid/v5';
+import { v5 as uuidv5 } from 'uuid';
 
 export default function (code: string): string {
   return `vnd_${uuidv5(code, '78530ab6-554f-46a9-bd72-8607b28560fb').replace(
